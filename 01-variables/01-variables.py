0e6f49e5284854abf8ac800427da791109bd7343
Edoardo Piccari <edoardopiccari@gmail.com>
2022-02-25 12:18:16 +0100

    Create 01-variables.py

---
+++ 01-variables/01-variables.py
@@ -0,0 +1,21 @@
+## Variables (Please nooooooooo!)
+
+x = 'somenthing'
+
+price = 9.99
+
+discount  = 0.2
+
+result = price * (1-discount) #Please stop
+
+print(result) 
+
+'''
+Output: 7.992 (Magicccccc!)
+'''
+
+name = 'Ciro'
+
+name = 'Gianni' #<- Already assign brah
+
+#You get the point :)
