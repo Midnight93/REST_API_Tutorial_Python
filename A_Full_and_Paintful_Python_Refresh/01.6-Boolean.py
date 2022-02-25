baf83e639a5aeb414ae8bdbba73e6d239ddf7573
Edoardo Piccari <edoardopiccari@gmail.com>
2022-02-25 12:51:43 +0100

    Create 01.6-Boolean.py

---
+++ A_Full_and_Paintful_Python_Refresh/01.6-Boolean.py
@@ -0,0 +1,17 @@
+print(5==5)
+
+#True
+
+print(4==5)
+
+#False
+
+friends = ['Ciro','Gennarino']
+
+abroad = ['Ciro', 'Gennarino']
+
+print(friends == abroad)
+
+print(friends is abroad)
+
+#Both True (WOW!!!)
