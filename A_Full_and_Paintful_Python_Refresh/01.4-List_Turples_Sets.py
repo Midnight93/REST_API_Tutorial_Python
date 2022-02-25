af84012a0e4b6a2f5c3736ff3369823ce6142665
Edoardo Piccari <edoardopiccari@gmail.com>
2022-02-25 12:41:58 +0100

    Update 01.4-List_Turples_Sets.py

--- A_Full_and_Paintful_Python_Refresh/01.4-List_Turples_Sets.py
+++ A_Full_and_Paintful_Python_Refresh/01.4-List_Turples_Sets.py
@@ -1,6 +1,6 @@
 l = ['Ciro','Ugo','Nino'] #List
 
-t = ('Ciro','Ugo','Nino') #Tuples -> Non mutable :O
+t = ('Ciro','Ugo','Nino') #Tuples -> No mutable :O
 
 s = {'Ciro','Ugo','Nino'} #Set (order and no duplicates) -> EZ!
 
